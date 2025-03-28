e6d007be6df389c116b60b57c6102ff6bd34b822
Everton-1020 <vertonandrade2005@gmail.com>
2025-03-28 20:45:34 -0300

    sprint2.js

--- Sprint2/sprint2.js
+++ Sprint2/sprint2.js
@@ -27,7 +27,7 @@ var botao = document.getElementById('Return');
 
 botao.addEventListener('click', function() {
     // Redirecionando para outra página HTML
-    window.location.href = 'splint.html'; // Substitua 'pagina2.html' pelo nome do arquivo para o qual você deseja redirecionar
+    window.location.href = 'sprint1.html'; // Substitua 'pagina2.html' pelo nome do arquivo para o qual você deseja redirecionar
 });
 
 // Chama a função para exibir os contatos
